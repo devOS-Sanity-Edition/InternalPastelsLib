3db84ad31a0751477d069d1cb2c465a9aa3a18cb
Asoji <asoji-git@hiitsdevin.mozmail.com>
2024-04-05 00:13:58 -0700

    ✏️⬆️ vers bump + name styling

--- src/main/kotlin/gay/asoji/innerpastels/blocks/BlockExtensions.kt
+++ src/main/kotlin/gay/asoji/innerpastels/blocks/BlockExtensions.kt
@@ -1,19 +1,19 @@
 package gay.asoji.innerpastels.blocks
 
-import gay.asoji.innerpastels.blocks.Properties.PastelBlock
-import gay.asoji.innerpastels.blocks.Properties.PastelCarpet
-import gay.asoji.innerpastels.blocks.Properties.PastelFence
-import gay.asoji.innerpastels.blocks.Properties.PastelFenceGate
-import gay.asoji.innerpastels.blocks.Properties.PastelGlass
-import gay.asoji.innerpastels.blocks.Properties.PastelGlassPane
-import gay.asoji.innerpastels.blocks.Properties.PastelLeaves
-import gay.asoji.innerpastels.blocks.Properties.PastelLight
-import gay.asoji.innerpastels.blocks.Properties.PastelLogs
-import gay.asoji.innerpastels.blocks.Properties.PastelPowder
-import gay.asoji.innerpastels.blocks.Properties.PastelSlab
-import gay.asoji.innerpastels.blocks.Properties.PastelStair
-import gay.asoji.innerpastels.blocks.Properties.PastelWall
-import gay.asoji.innerpastels.blocks.Properties.PastelWool
+import gay.asoji.innerpastels.blocks.Properties.pastelBlock
+import gay.asoji.innerpastels.blocks.Properties.pastelCarpet
+import gay.asoji.innerpastels.blocks.Properties.pastelFence
+import gay.asoji.innerpastels.blocks.Properties.pastelFenceGate
+import gay.asoji.innerpastels.blocks.Properties.pastelGlass
+import gay.asoji.innerpastels.blocks.Properties.pastelGlassPane
+import gay.asoji.innerpastels.blocks.Properties.pastelLeaves
+import gay.asoji.innerpastels.blocks.Properties.pastelLight
+import gay.asoji.innerpastels.blocks.Properties.pastelLogs
+import gay.asoji.innerpastels.blocks.Properties.pastelPowder
+import gay.asoji.innerpastels.blocks.Properties.pastelSlab
+import gay.asoji.innerpastels.blocks.Properties.pastelStair
+import gay.asoji.innerpastels.blocks.Properties.pastelWall
+import gay.asoji.innerpastels.blocks.Properties.pastelWool
 import gay.asoji.innerpastels.crab.CrabInTheCode
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry
 import net.minecraft.core.Registry
@@ -23,6 +23,7 @@ import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.DyeColor
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.block.*
+import net.minecraft.world.level.block.state.BlockBehaviour
 import net.minecraft.world.level.material.MapColor
 
 /**
@@ -70,7 +71,7 @@ fun Block.registerBlockWithItem(modID: String, name: String): Block {
  * @return [Block]
  */
 fun DyeColor.registerGlassBlock(modID: String, name: String): Block =
-    StainedGlassBlock(this, PastelGlass().mapColor(this)).registerBlock(modID, name)
+    StainedGlassBlock(this, pastelGlass().mapColor(this)).registerBlock(modID, name)
 
 /**
  * Registers a stained-glass pane block copying the settings of white stained-glass with the color based on a [DyeColor]
@@ -79,7 +80,7 @@ fun DyeColor.registerGlassBlock(modID: String, name: String): Block =
  * @return [Block]
  */
 fun DyeColor.registerGlassPaneBlock(modID: String, name: String): Block =
-    StainedGlassPaneBlock(this, PastelGlassPane().mapColor(this)).registerBlock(modID, name)
+    StainedGlassPaneBlock(this, pastelGlassPane().mapColor(this)).registerBlock(modID, name)
 
 /**
  * Registers a pastel block with an accompanying item with the color based on a [DyeColor]
@@ -87,7 +88,7 @@ fun DyeColor.registerGlassPaneBlock(modID: String, name: String): Block =
  * @property name the ID of the block
  * @return [Block]
  */
-fun DyeColor.registerPastelBlock(modID: String, name: String): Block = Block(PastelBlock().mapColor(this)).registerBlockWithItem(modID, name)
+fun DyeColor.registerPastelBlock(modID: String, name: String): Block = Block(pastelBlock().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers a pastel block with an accompanying item with the color based on a [MapColor]
@@ -95,7 +96,7 @@ fun DyeColor.registerPastelBlock(modID: String, name: String): Block = Block(Pas
  * @property name the ID of the block
  * @return [Block]
  */
-fun MapColor.registerPastelBlock(modID: String, name: String): Block = Block(PastelBlock().mapColor(this)).registerBlockWithItem(modID, name)
+fun MapColor.registerPastelBlock(modID: String, name: String): Block = Block(pastelBlock().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers a pastel powder block with an accompanying item with the color based on a [DyeColor], paired with an accompanying normal pastel block when touched with water
@@ -105,7 +106,7 @@ fun MapColor.registerPastelBlock(modID: String, name: String): Block = Block(Pas
  * @return [ConcretePowderBlock]
  */
 fun DyeColor.registerPastelPowderBlock(modID: String, name: String, resultPastelBlock: Block): Block =
-    ConcretePowderBlock(resultPastelBlock, PastelPowder().mapColor(this)).registerBlockWithItem(modID, name)
+    ConcretePowderBlock(resultPastelBlock, pastelPowder().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers a pastel powder block with an accompanying item with the color based on a [MapColor], paired with an accompanying normal pastel block when touched with water
@@ -115,7 +116,7 @@ fun DyeColor.registerPastelPowderBlock(modID: String, name: String, resultPastel
  * @return [ConcretePowderBlock]
  */
 fun MapColor.registerPastelPowderBlock(modID: String, name: String, resultPastelBlock: Block): Block =
-    ConcretePowderBlock(resultPastelBlock, PastelPowder().mapColor(this)).registerBlockWithItem(modID, name)
+    ConcretePowderBlock(resultPastelBlock, pastelPowder().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers a pastel fence block with an accompanying item with a chosen [DyeColor] to be shown on a map
@@ -124,7 +125,7 @@ fun MapColor.registerPastelPowderBlock(modID: String, name: String, resultPastel
  * @return [FenceBlock]
  */
 fun DyeColor.registerPastelFenceBlock(modID: String, name: String): Block =
-    FenceBlock(PastelFence().mapColor(this)).registerBlockWithItem(modID, name)
+    FenceBlock(pastelFence().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers a pastel fence block with an accompanying item with a chosen [MapColor] to be shown on a map
@@ -133,7 +134,7 @@ fun DyeColor.registerPastelFenceBlock(modID: String, name: String): Block =
  * @return [FenceBlock]
  */
 fun MapColor.registerPastelFenceBlock(modID: String, name: String): Block =
-    FenceBlock(PastelFence().mapColor(this)).registerBlockWithItem(modID, name)
+    FenceBlock(pastelFence().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel fence gate block with an accompanying item with a chosen [DyeColor] to be shown on a map
@@ -142,7 +143,7 @@ fun MapColor.registerPastelFenceBlock(modID: String, name: String): Block =
  * @return [FenceGateBlock]
  */
 fun DyeColor.registerPastelFenceGateBlock(modID: String, name: String): Block =
-    FenceGateBlock(PastelFenceGate().mapColor(this)).registerBlockWithItem(modID, name)
+    FenceGateBlock(pastelFenceGate().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel fence gate block with an accompanying item with a chosen [MapColor] to be shown on a map
@@ -151,7 +152,7 @@ fun DyeColor.registerPastelFenceGateBlock(modID: String, name: String): Block =
  * @return [FenceGateBlock]
  */
 fun MapColor.registerPastelFenceGateBlock(modID: String, name: String): Block =
-    FenceGateBlock(PastelFenceGate().mapColor(this)).registerBlockWithItem(modID, name)
+    FenceGateBlock(pastelFenceGate().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel wall block with an accompanying item with a chosen [DyeColor] to be shown on a map
@@ -160,7 +161,7 @@ fun MapColor.registerPastelFenceGateBlock(modID: String, name: String): Block =
  * @return [WallBlock]
  */
 fun DyeColor.registerPastelWallBlock(modID: String, name: String): Block =
-    WallBlock(PastelWall().mapColor(this)).registerBlockWithItem(modID, name)
+    WallBlock(pastelWall().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel wall block with an accompanying item with a chosen [MapColor] to be shown on a map
@@ -169,7 +170,7 @@ fun DyeColor.registerPastelWallBlock(modID: String, name: String): Block =
  * @return [WallBlock]
  */
 fun MapColor.registerPastelWallBlock(modID: String, name: String): Block =
-    WallBlock(PastelWall().mapColor(this)).registerBlockWithItem(modID, name)
+    WallBlock(pastelWall().mapColor(this)).registerBlockWithItem(modID, name)
 
 // i'm going to cry
 // fucking same bestie
@@ -182,7 +183,7 @@ fun MapColor.registerPastelWallBlock(modID: String, name: String): Block =
  * @return [SlabBlock]
  */
 fun DyeColor.registerPastelSlabBlock(modID: String, name: String): Block =
-    SlabBlock(PastelSlab().mapColor(this)).registerBlockWithItem(modID, name)
+    SlabBlock(pastelSlab().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel slab block with an accompanying item with a chosen [MapColor] to be shown on a map
@@ -191,7 +192,7 @@ fun DyeColor.registerPastelSlabBlock(modID: String, name: String): Block =
  * @return [SlabBlock]
  */
 fun MapColor.registerPastelSlabBlock(modID: String, name: String): Block =
-    SlabBlock(PastelSlab().mapColor(this)).registerBlockWithItem(modID, name)
+    SlabBlock(pastelSlab().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel stair block with an accompanying item with a chosen [DyeColor] to be shown on a map
@@ -201,7 +202,7 @@ fun MapColor.registerPastelSlabBlock(modID: String, name: String): Block =
  */
 fun DyeColor.registerPastelStairsBlock(modID: String, name: String): Block = StairBlock(
     Blocks.STONE_STAIRS.defaultBlockState(),
-    PastelStair().mapColor(this)
+    pastelStair().mapColor(this)
 ).registerBlockWithItem(modID, name)
 
 /**
@@ -212,7 +213,7 @@ fun DyeColor.registerPastelStairsBlock(modID: String, name: String): Block = Sta
  */
 fun MapColor.registerPastelStairsBlock(modID: String, name: String): Block = StairBlock(
     Blocks.STONE_STAIRS.defaultBlockState(),
-    PastelStair().mapColor(this)
+    pastelStair().mapColor(this)
 ).registerBlockWithItem(modID, name)
 
 /**
@@ -221,7 +222,7 @@ fun MapColor.registerPastelStairsBlock(modID: String, name: String): Block = Sta
  * @property name the ID of the block
  * @return [Block]
  */
-fun DyeColor.registerWoolBlock(modID: String, name: String): Block = Block(PastelWool().mapColor(this)).registerBlockWithItem(modID, name)
+fun DyeColor.registerWoolBlock(modID: String, name: String): Block = Block(pastelWool().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel wool block with an accompanying item with a chosen [MapColor] to be shown on a map
@@ -229,7 +230,7 @@ fun DyeColor.registerWoolBlock(modID: String, name: String): Block = Block(Paste
  * @property name the ID of the block
  * @return [Block]
  */
-fun MapColor.registerWoolBlock(modID: String, name: String): Block = Block(PastelWool().mapColor(this)).registerBlockWithItem(modID, name)
+fun MapColor.registerWoolBlock(modID: String, name: String): Block = Block(pastelWool().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel light block with an accompanying item with a chosen [DyeColor] to be shown on a map
@@ -237,7 +238,7 @@ fun MapColor.registerWoolBlock(modID: String, name: String): Block = Block(Paste
  * @property name the ID of the block
  * @return [Block]
  */
-fun DyeColor.registerLightBlock(modID: String, name: String): Block = Block(PastelLight().mapColor(this)).registerBlockWithItem(modID, name)
+fun DyeColor.registerLightBlock(modID: String, name: String): Block = Block(pastelLight().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel light block with an accompanying item with a chosen [MapColor] to be shown on a map
@@ -245,7 +246,7 @@ fun DyeColor.registerLightBlock(modID: String, name: String): Block = Block(Past
  * @property name the ID of the block
  * @return [Block]
  */
-fun MapColor.registerLightBlock(modID: String, name: String): Block = Block(PastelLight().mapColor(this)).registerBlockWithItem(modID, name)
+fun MapColor.registerLightBlock(modID: String, name: String): Block = Block(pastelLight().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * @suppress
@@ -259,7 +260,7 @@ object crab1 { val crab2 = CrabInTheCode.crabDeezNuts("hey, it felt comfortable 
  * @return [Block]
  */
 fun DyeColor.registerCarpetBlock(modID: String, name: String): Block =
-    CarpetBlock(PastelCarpet().mapColor(this)).registerBlockWithItem(modID, name)
+    CarpetBlock(pastelCarpet().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel carpet block with an accompanying item with a chosen [MapColor] to be shown on a map
@@ -268,7 +269,7 @@ fun DyeColor.registerCarpetBlock(modID: String, name: String): Block =
  * @return [Block]
  */
 fun MapColor.registerCarpetBlock(modID: String, name: String): Block =
-    CarpetBlock(PastelCarpet().mapColor(this)).registerBlockWithItem(modID, name)
+    CarpetBlock(pastelCarpet().mapColor(this)).registerBlockWithItem(modID, name)
 
 /**
  * Registers the pastel log blocks with an accompanying item with a chosen [MapColor] to be shown on a map\
@@ -277,14 +278,14 @@ fun MapColor.registerCarpetBlock(modID: String, name: String): Block =
  * @return [RotatedPillarBlock]
  */
 fun MapColor.registerLogBlock(modID: String, name: String): Block =
-    RotatedPillarBlock(PastelLogs().mapColor(this)).registerBlockWithItem(modID, name).apply { FlammableBlockRegistry.getDefaultInstance().add(this, 5, 5) }
+    RotatedPillarBlock(pastelLogs().mapColor(this)).registerBlockWithItem(modID, name).apply { FlammableBlockRegistry.getDefaultInstance().add(this, 5, 5) }
 
 // TODO: Document this shit
 fun DyeColor.registerLogBlock(modID: String, name: String): Block =
-    RotatedPillarBlock(PastelLogs().mapColor(this)).registerBlockWithItem(modID, name).apply { FlammableBlockRegistry.getDefaultInstance().add(this, 5, 5) }
+    RotatedPillarBlock(pastelLogs().mapColor(this)).registerBlockWithItem(modID, name).apply { FlammableBlockRegistry.getDefaultInstance().add(this, 5, 5) }
 
-fun MapColor.registerLeavesBlock(modID: String, name: String): Block =
-    LeavesBlock(PastelLeaves().mapColor(this)).registerBlockWithItem(modID, name).apply { FlammableBlockRegistry.getDefaultInstance().add(this, 30, 60) }
+fun MapColor.registerLeavesBlock(modID: String, name: String, emissiveRenderingState: BlockBehaviour.StatePredicate): Block =
+    LeavesBlock(pastelLeaves().mapColor(this).hasPostProcess(emissiveRenderingState).emissiveRendering(emissiveRenderingState)).registerBlockWithItem(modID, name).apply { FlammableBlockRegistry.getDefaultInstance().add(this, 30, 60) }
 
-fun DyeColor.registerLeavesBlock(modID: String, name: String): Block =
-    LeavesBlock(PastelLeaves().mapColor(this)).registerBlockWithItem(modID, name).apply { FlammableBlockRegistry.getDefaultInstance().add(this, 30, 60) }
+fun DyeColor.registerLeavesBlock(modID: String, name: String, emissiveRenderingState: BlockBehaviour.StatePredicate): Block =
+    LeavesBlock(pastelLeaves().mapColor(this).hasPostProcess(emissiveRenderingState).emissiveRendering(emissiveRenderingState)).registerBlockWithItem(modID, name).apply { FlammableBlockRegistry.getDefaultInstance().add(this, 30, 60) }
--- src/main/kotlin/gay/asoji/innerpastels/blocks/Properties.kt
+++ src/main/kotlin/gay/asoji/innerpastels/blocks/Properties.kt
@@ -2,9 +2,7 @@ package gay.asoji.innerpastels.blocks
 
 import gay.asoji.innerpastels.crab.CrabInTheCode
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.block.LeavesBlock
 import net.minecraft.world.level.block.state.BlockBehaviour
 
 /**
@@ -14,21 +12,21 @@ object Properties {
     /**
      * Default properties for a Pastel Block, being a copy of WHITE_CONCRETE
      */
-    fun PastelBlock(): BlockBehaviour.Properties {
+    fun pastelBlock(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.WHITE_CONCRETE)
     }
 
     /**
      * Default properties for a Pastel Powder Block, being a copy of WHITE_CONCRETE_POWDER
      */
-    fun PastelPowder(): BlockBehaviour.Properties {
+    fun pastelPowder(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.WHITE_CONCRETE_POWDER)
     }
 
     /**
      * Default properties for a Pastel Glass Pane, being a copy of WHITE_STAINED_GLASS, with vision not being blocked, and can't suffocate
      */
-    fun PastelGlassPane(): BlockBehaviour.Properties {
+    fun pastelGlassPane(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.WHITE_STAINED_GLASS_PANE)
             .blockVision(Blocks::never)
             .suffocates(Blocks::never)
@@ -37,77 +35,77 @@ object Properties {
     /**
      * Default properties for a Pastel Fence, being a copy of NETHER_BRICK_FENCE
      */
-    fun PastelFence(): BlockBehaviour.Properties {
+    fun pastelFence(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.NETHER_BRICK_FENCE)
     }
 
     /**
      * Default properties for a Pastel Fence Gate, being a copy of Stone's properties, will probably get reworked in the future.
      */
-    fun PastelFenceGate(): FabricBlockSettings {
+    fun pastelFenceGate(): FabricBlockSettings {
         return FabricBlockSettings.copyOf(Blocks.STONE)
     }
 
     /**
      * Default properties for a Pastel Wall, being a copy of STONE_BRICK_WALL
      */
-    fun PastelWall(): BlockBehaviour.Properties {
+    fun pastelWall(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.STONE_BRICK_WALL)
     }
 
     /**
      * Default properties for a Pastel Slab, being a copy of STONE_SLAB
      */
-    fun PastelSlab(): BlockBehaviour.Properties {
+    fun pastelSlab(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.STONE_SLAB)
     }
 
     /**
      * Default properties for a Pastel Stair, being a copy of STONE_STAIRS
      */
-    fun PastelStair(): BlockBehaviour.Properties {
+    fun pastelStair(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.STONE_STAIRS)
     }
 
     /**
      * Default properties for a Pastel Wool, being a copy of WHITE_WOOL
      */
-    fun PastelWool(): BlockBehaviour.Properties {
+    fun pastelWool(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.WHITE_WOOL)
     }
 
     /**
      * Default properties for a Pastel Light, being a copy of GLOWSTONE
      */
-    fun PastelLight(): BlockBehaviour.Properties {
+    fun pastelLight(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.GLOWSTONE)
     }
 
     /**
      * Default properties for a Pastel Carpet, being a copy of WHITE_CARPET
      */
-    fun PastelCarpet(): BlockBehaviour.Properties {
+    fun pastelCarpet(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.WHITE_CARPET)
     }
 
     /**
      * Default properties for a Pastel Glass, being a copy of WHITE_STAINED_GLASS
      */
-    fun PastelGlass(): BlockBehaviour.Properties {
+    fun pastelGlass(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.WHITE_STAINED_GLASS)
             .blockVision(Blocks::never)
             .suffocates(Blocks::never)
     }
 
     // **UNUSED**
-    fun PastelCrops(): BlockBehaviour.Properties {
+    fun pastelCrops(): BlockBehaviour.Properties {
         return (FabricBlockSettings.copyOf(Blocks.WHEAT))
     }
 
     /**
      * Default properties for a Pastel Leaves, being a copy of OAK_LEAVES
      */
-    fun PastelLeaves(): BlockBehaviour.Properties {
+    fun pastelLeaves(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.OAK_LEAVES)
             .blockVision(Blocks::never)
             .suffocates(Blocks::never)
@@ -116,14 +114,14 @@ object Properties {
     /**
      * Default properties for a Pastel Log, being a copy of OAK_LOG
      */
-    fun PastelLogs(): BlockBehaviour.Properties {
+    fun pastelLogs(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.OAK_LOG)
     }
 
     /**
      * Default properties for a Pastel Plank, being a copy of OAK_PLANKS
      */
-    fun PastelPlanks(): BlockBehaviour.Properties {
+    fun pastelPlanks(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.OAK_PLANKS)
     }
 
@@ -135,7 +133,7 @@ object Properties {
     /**
      * Default properties for a Pastel Sand, being a copy of SAND
      */
-    fun PastelSand(): BlockBehaviour.Properties {
+    fun pastelSand(): BlockBehaviour.Properties {
         return FabricBlockSettings.copyOf(Blocks.SAND)
     }
 }
